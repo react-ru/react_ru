6feb3fe9d5f8081243c4d6e47846759fd2e227b8
Yury Chebotaev <yury.79120345101@gmail.com>
2019-01-05 22:41:01 +0300

    Add NotFound page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router";
 import { Main } from "../pages/Main";
+// import { CodeReview } from '../pages/CodeReview';
+import { NotFound } from '../pages/NotFound';
 
 export const App = () => {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Main} />
+        {/* <Route exact path="/code-review" component={CodeReview} /> */}
+        <Route path="*" component={NotFound} />
       </Switch>
     </div>
   );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NotFound.css";
+
+export const NotFound = () => {
+  return (
+    <div className="NotFound">
+      <div className="NotFound__first_line">404</div>
+      <div className="NotFound__second_line">Not Found</div>
+    </div>
+  )
+};
